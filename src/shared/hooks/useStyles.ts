cc2c15026a51a26cc8edba51f5bea954c8dc6b67
ahsan-javaid <ahsan786cs@gmail.com>
2022-01-09 18:03:53 +0500

    feature: add custom hooks

---
+++ src/shared/hooks/useStyles.ts
@@ -0,0 +1,8 @@
+export const useCardStyles = () => ({
+  card: { width: 300, margin: 10},
+  meta: { marginBottom: 5},
+  avatar: { maxHeight: 200, background: '#F5F5F5' },
+  heart: { color: '#eb2f96'},
+  actions: { background: '#FAFAFA'},
+  body: { background: '#F5F5F5'},
+});
